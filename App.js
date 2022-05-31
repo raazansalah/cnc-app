24cdf364ea64430f270e952172b7b735e501ec3d
razan salah <razansalah022@gmail.com>
2022-05-31 15:14:28 +0200

    done

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import axios from "axios";
+import { StyleSheet, Text, View, Button } from "react-native";
+const clickHandler = (num) => {
+  console.log(num);
+  // return axios.post("http://localhost:3000/val", {
+  //   "val":"num"
+  // });
+  // .then(async (res) => {
+  //   console.log(res);
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.btn}>
+        <Button
+          title="circle"
+          onPress={() => clickHandler("0")}
+          color="#ffffff"
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button
+          title="square"
+          onPress={() => clickHandler("1")}
+          color="#ffffff"
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button
+          title="rectangle"
+          onPress={() => clickHandler("2")}
+          color="#ffffff"
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +44,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    backgroundColor: "red",
+    margin: 15,
+    borderRadius: 15,
+    width: 150,
   },
 });
